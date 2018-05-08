eca240205297360748e56c2d1db8c4d4a81d4eeb
Observer-L <observer_l@163.com>
2018-05-08 15:21:29 +0800

    fixed problem of animation when double click the panel

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -1,13 +1,13 @@
 const panels = document.querySelectorAll('.panel');
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+// panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 function toggleOpen(e) {
   this.classList.toggle('panel-actived');
 }
 
-function toggleActive(e) {
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('open-active');
-  }
-}
+// function toggleActive(e) {
+//   if (e.propertyName.includes('flex')) {
+//     this.classList.toggle('open-active');
+//   }
+// }
